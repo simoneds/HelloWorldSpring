c9e9766e74df1c60011bd02cdc7425178ad175ce
unknown <simone.distefano@outlook.it>
2015-03-24 17:32:00 +0100

    aggiunti form e somma.java

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -4,11 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class GreetingController {
 
-    @RequestMapping("/greeting")
+
+    @RequestMapping("/form")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
---
+++ src/main/java/hello/Somma.java
@@ -0,0 +1,26 @@
+package hello;
+
+/**
+ * Created by Simone on 24/03/2015.
+ */
+public class Somma {
+    private int add1;
+    private int add2;
+
+    public int getAdd1 () {
+        return this.add1;
+    }
+
+    public void setAdd1 (int add1) {
+        this.add1 = add1;
+    }
+
+    public int getAdd2 () {
+        return this.add2;
+    }
+
+    public void setAdd2 (int add2) {
+        this.add2 = add2;
+    }
+
+}
