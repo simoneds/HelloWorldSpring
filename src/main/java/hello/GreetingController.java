ae77fcc1ab5008632b492a4149906ff6d1686fd8
unknown <simone.distefano@outlook.it>
2015-03-25 15:26:55 +0100

    modifiche e aggiunti file per prodotto

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -9,9 +9,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class GreetingController {
 
-    @RequestMapping("/pippo")
-    public String metodo() {
-        return "pluto";
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
 
     @RequestMapping("/form")
@@ -25,10 +26,13 @@ public class GreetingController {
         return "risultato";
     }
 
-    @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+    @RequestMapping("/moltiplica")
+    public String moltiplica(Prodotto prodotto) {
+        return "moltiplica";
     }
 
+    @RequestMapping("/metodoAzione")
+    public String metodoAzione(Prodotto prodotto) {
+        return "risultatoprodotto";
+    }
 }
